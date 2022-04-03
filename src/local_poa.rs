0f21621d8997beeb42f3c32d0cb1b0615ee0f675
dcmonti <d.monti11@campus.unimib.it>
2022-04-03 09:22:24 +0200

    tests for local poa

--- src/local_poa.rs
+++ src/local_poa.rs
@@ -1,6 +1,5 @@
 use std::{cmp::Ordering, collections::HashMap};
 
-
 use crate::{basic_output, graph::LnzGraph};
 
 pub fn exec(
@@ -14,13 +13,13 @@ pub fn exec(
 
     let mut m = vec![vec![0; sequence.len()]; lnz.len()];
     let mut path = vec![vec![('x', 0); sequence.len()]; lnz.len()];
-    let (mut best_row, mut best_col) = (0,0);
-    for i in 0..lnz.len() - 1{
+    let (mut best_row, mut best_col) = (0, 0);
+    for i in 0..lnz.len() - 1 {
         for j in 0..sequence.len() - 1 {
             match (i, j) {
                 (0, _) | (_, 0) => path[i][j] = ('O', 0),
                 _ => {
-                    let l = m[i][j-1] + scores_matrix.get(&(sequence[j], '-')).unwrap();
+                    let l = m[i][j - 1] + scores_matrix.get(&(sequence[j], '-')).unwrap();
                     let l_idx = i;
 
                     let mut d;
@@ -29,11 +28,11 @@ pub fn exec(
                     let mut u;
                     let u_idx;
                     if !nodes_with_pred[i] {
-                        d = m[i-1][j-1] + scores_matrix.get(&(sequence[j], lnz[i])).unwrap();
-                        d_idx = i-1;
+                        d = m[i - 1][j - 1] + scores_matrix.get(&(sequence[j], lnz[i])).unwrap();
+                        d_idx = i - 1;
 
-                        u = m[i-1][j] + scores_matrix.get(&('-', lnz[i])).unwrap();
-                        u_idx = i-1;
+                        u = m[i - 1][j] + scores_matrix.get(&('-', lnz[i])).unwrap();
+                        u_idx = i - 1;
                     } else {
                         (d, d_idx) = get_best_d(&m, pred_hash.get(&i).unwrap(), j);
                         (u, u_idx) = get_best_u(&m, pred_hash.get(&i).unwrap(), j);
@@ -50,39 +49,33 @@ pub fn exec(
                         }
                         m[i][j] = best_val;
                         path[i][j] = match dir {
-                            'D'|'d' => {
-                                (dir, d_idx)
-                            }
-                            'U' => {
-                                (dir, u_idx)
-                            }
-                            _ => {
-                                (dir, l_idx)
-                            }
+                            'D' | 'd' => (dir, d_idx),
+                            'U' => (dir, u_idx),
+                            _ => (dir, l_idx),
                         }
                     }
                 }
             }
-            
+
             if m[i][j] > m[best_row][best_col] {
                 best_row = i;
                 best_col = j;
             }
         }
     }
-    
+
     println!("Best alignment: {}", m[best_row][best_col]);
 
     basic_output::write_align_local_poa(&path, sequence, lnz, best_row, best_col);
     m[best_row][best_col]
 }
 
-fn get_best_d(m: &[Vec<i32>], p_arr: &[usize], j: usize) -> (i32, usize){
+fn get_best_d(m: &[Vec<i32>], p_arr: &[usize], j: usize) -> (i32, usize) {
     let mut d = 0;
     let mut d_idx = 0;
     let mut first = false;
-    for p in p_arr{
-        let current_d = m[*p][j-1];
+    for p in p_arr {
+        let current_d = m[*p][j - 1];
         if first {
             first = false;
             d = current_d;
@@ -96,11 +89,11 @@ fn get_best_d(m: &[Vec<i32>], p_arr: &[usize], j: usize) -> (i32, usize){
     (d, d_idx)
 }
 
-fn get_best_u(m: &[Vec<i32>], p_arr: &[usize], j: usize) -> (i32, usize){
+fn get_best_u(m: &[Vec<i32>], p_arr: &[usize], j: usize) -> (i32, usize) {
     let mut u = 0;
     let mut u_idx = 0;
     let mut first = false;
-    for p in p_arr{
+    for p in p_arr {
         let current_u = m[*p][j];
         if first {
             first = false;
@@ -117,25 +110,84 @@ fn get_best_u(m: &[Vec<i32>], p_arr: &[usize], j: usize) -> (i32, usize){
 
 fn get_best_d_u_l(d: i32, u: i32, l: i32) -> (i32, char) {
     match d.cmp(&u) {
-        Ordering::Less => {
-            match u.cmp(&l) {
-                Ordering::Less => {
-                    (l, 'L')
-                }
-                _ => {
-                    (u, 'U')
-                }
-            }
+        Ordering::Less => match u.cmp(&l) {
+            Ordering::Less => (l, 'L'),
+            _ => (u, 'U'),
         },
-        _ => {
-            match d.cmp(&l) {
-                Ordering::Less => {
-                    (l, 'L')
+        _ => match d.cmp(&l) {
+            Ordering::Less => (l, 'L'),
+            _ => (d, 'D'),
+        },
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashMap;
+
+    use bit_vec::BitVec;
+
+    use crate::graph::LnzGraph;
+
+    #[test]
+    fn test_local_poa_consider_substrings() {
+        let s = vec!['$', 'A', 'A', 'C', 'C', 'C', 'A', 'A'];
+
+        let lnz = vec!['$', 'G', 'G', 'C', 'C', 'C', 'G', 'G', 'F'];
+        let mut nwp = BitVec::from_elem(lnz.len(), false);
+        nwp.set(1, true);
+        nwp.set(8, true);
+        let mut pred_hash = HashMap::new();
+        pred_hash.insert(1, vec![0]);
+        pred_hash.insert(8, vec![7]);
+        let graph_struct = LnzGraph {
+            lnz,
+            nwp,
+            pred_hash,
+        };
+        let mut score_matrix = HashMap::new();
+        for c1 in ['A', 'C', 'G', '-'] {
+            for c2 in ['A', 'C', 'G', '-'] {
+                if c1 == c2 {
+                    score_matrix.insert((c1, c2), 1);
+                } else {
+                    score_matrix.insert((c1, c2), -1);
                 }
-                _ => {
-                    (d, 'D')
-                } 
             }
         }
+        let align_score = super::exec(&s, &graph_struct, &score_matrix);
+        assert_eq!(align_score, 3);
+    }
+
+    #[test]
+    fn local_poa_consider_best_predecessor() {
+        let s = vec!['$', 'A', 'A', 'C', 'C', 'C', 'A', 'A'];
+
+        let lnz = vec!['$', 'G', 'G', 'G', 'C', 'C', 'C', 'G', 'G', 'F'];
+        let mut nwp = BitVec::from_elem(lnz.len(), false);
+        nwp.set(1, true);
+        nwp.set(6, true);
+        nwp.set(9, true);
+        let mut pred_hash = HashMap::new();
+        pred_hash.insert(1, vec![0]);
+        pred_hash.insert(6, vec![3]);
+        pred_hash.insert(9, vec![8, 5]);
+        let graph_struct = LnzGraph {
+            lnz,
+            nwp,
+            pred_hash,
+        };
+        let mut score_matrix = HashMap::new();
+        for c1 in ['A', 'C', 'G', '-'] {
+            for c2 in ['A', 'C', 'G', '-'] {
+                if c1 == c2 {
+                    score_matrix.insert((c1, c2), 1);
+                } else {
+                    score_matrix.insert((c1, c2), -1);
+                }
+            }
+        }
+        let align_score = super::exec(&s, &graph_struct, &score_matrix);
+        assert_eq!(align_score, 2);
     }
 }
