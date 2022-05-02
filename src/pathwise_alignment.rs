9ef2f91204522fdd37d95d315125e3fd9ef0bbf1
dcmonti <d.monti11@campus.unimib.it>
2022-05-02 11:37:20 +0200

    changed recursion in pathwise alignement

--- src/pathwise_alignment.rs
+++ src/pathwise_alignment.rs
@@ -1,7 +1,7 @@
 use crate::{bitfield_path as bf, graph::LnzGraph};
 use bitvec::prelude::*;
 use std::collections::{HashMap, HashSet};
-// TODO: edge must be in at least 1 path to be considered
+// FIXME: 
 pub fn exec(
     sequence: &[char],
     graph: &LnzGraph,
@@ -17,7 +17,7 @@ pub fn exec(
     let mut path = vec![vec![bitvec![u16, Msb0; 0; 32]; sequence.len()]; lnz.len()];
 
     let mut alphas = vec![path_number + 1; lnz.len()];
-    
+
     for i in 0..lnz.len() - 1 {
         for j in 0..sequence.len() {
             match (i, j) {
@@ -29,26 +29,29 @@ pub fn exec(
                 (_, 0) => {
                     if !nodes_with_pred[i] {
                         let curr_node_paths = path_node[i].iter().collect::<HashSet<&usize>>();
-                        let pred_paths = path_node[i-1].iter().collect::<HashSet<_>>();
+                        let pred_paths = path_node[i - 1].iter().collect::<HashSet<_>>();
                         let x = curr_node_paths
                             .intersection(&pred_paths)
                             .collect::<Vec<_>>();
-                        if x.contains(&&&alphas[i-1]) {
-                            alphas[i] = alphas[i-1];
+                        if x.contains(&&&alphas[i - 1]) {
+                            alphas[i] = alphas[i - 1];
                             for path in x.iter() {
                                 if ***path == alphas[i] {
-                                    dpm[i][j][***path] = dpm[i-1][j][***path] + score_matrix.get(&(lnz[i], '-')).unwrap();
+                                    dpm[i][j][***path] = dpm[i - 1][j][***path]
+                                        + score_matrix.get(&(lnz[i], '-')).unwrap();
                                 } else {
-                                    dpm[i][j][***path] = dpm[i-1][j][***path];
+                                    dpm[i][j][***path] = dpm[i - 1][j][***path];
                                 }
                             }
-
                         } else {
                             alphas[i] = ***x.iter().min().unwrap();
-                            dpm[i][j][alphas[i]] = dpm[i-1][j][alphas[i]] + dpm[i-1][j][alphas[i-1]] + score_matrix.get(&(lnz[i], '-')).unwrap();
+                            dpm[i][j][alphas[i]] = dpm[i - 1][j][alphas[i]]
+                                + dpm[i - 1][j][alphas[i - 1]]
+                                + score_matrix.get(&(lnz[i], '-')).unwrap();
                             for path in x.iter() {
                                 if ***path != alphas[i] {
-                                    dpm[i][j][***path] = dpm[i-1][j][***path] - dpm[i-1][j][alphas[i]]
+                                    dpm[i][j][***path] =
+                                        dpm[i - 1][j][***path] - dpm[i - 1][j][alphas[i]]
                                 }
                             }
                         }
@@ -70,8 +73,9 @@ pub fn exec(
                             if x.contains(&&&alphas[*p]) {
                                 let paths = x.iter().map(|p| ***p).collect::<Vec<usize>>();
                                 alphas_deltas.insert(alphas[*p], paths);
-                                
-                                dpm[i][j][alphas[*p]] = dpm[*p][j][alphas[*p]] + score_matrix.get(&(lnz[i], '-')).unwrap();
+
+                                dpm[i][j][alphas[*p]] = dpm[*p][j][alphas[*p]]
+                                    + score_matrix.get(&(lnz[i], '-')).unwrap();
                                 for path in x.iter() {
                                     if path != &&&alphas[*p] {
                                         dpm[i][j][***path] = dpm[*p][j][***path];
@@ -82,11 +86,14 @@ pub fn exec(
                                 let temp_alpha = x.iter().min().unwrap();
                                 let paths = x.iter().map(|p| ***p).collect::<Vec<usize>>();
                                 alphas_deltas.insert(***temp_alpha, paths);
-                                
-                                dpm[i][j][***temp_alpha] = dpm[*p][j][alphas[*p]] + dpm[*p][j][***temp_alpha] + score_matrix.get(&(lnz[i], '-')).unwrap();
+
+                                dpm[i][j][***temp_alpha] = dpm[*p][j][alphas[*p]]
+                                    + dpm[*p][j][***temp_alpha]
+                                    + score_matrix.get(&(lnz[i], '-')).unwrap();
                                 for path in x.iter() {
                                     if path != temp_alpha {
-                                        dpm[i][j][***path] = dpm[*p][j][***path] - dpm[*p][j][***temp_alpha];
+                                        dpm[i][j][***path] =
+                                            dpm[*p][j][***path] - dpm[*p][j][***temp_alpha];
                                     }
                                 }
                             }
@@ -98,7 +105,7 @@ pub fn exec(
                                     dpm[i][j][*a] -= dpm[i][j][alphas[i]];
                                     for path in delta.iter() {
                                         if path != a {
-                                            dpm[i][j][*path] -= dpm[i][j][*a];
+                                            dpm[i][j][*path] += dpm[i][j][*a];
                                         }
                                     }
                                 }
@@ -113,6 +120,75 @@ pub fn exec(
                         dpm[i][j][k] = dpm[i][j - 1][k];
                     }
                     path[i][j] = bf::set_path_cell(i, 'L');
+                }
+                _ => {
+                    if !nodes_with_pred[i] {
+                        let curr_node_paths = path_node[i].iter().collect::<HashSet<&usize>>();
+                        let pred_paths = path_node[i - 1].iter().collect::<HashSet<_>>();
+                        let x = curr_node_paths
+                            .intersection(&pred_paths)
+                            .collect::<Vec<_>>();
+                        if x.contains(&&&alphas[i - 1]) {
+                            let u = dpm[i - 1][j][alphas[i - 1]]
+                                + score_matrix.get(&(lnz[i], '-')).unwrap();
+                            let d = dpm[i - 1][j - 1][alphas[i - 1]]
+                                + score_matrix.get(&(lnz[i], sequence[j])).unwrap();
+                            let l = dpm[i][j - 1][alphas[i]]
+                                + score_matrix.get(&(sequence[j], '-')).unwrap();
+                            dpm[i][j][alphas[i]] = *[d, u, l].iter().max().unwrap();
+                            for path in x.iter() {
+                                if ***path != alphas[i] {
+                                    if dpm[i][j][alphas[i]] == d {
+                                        dpm[i][j][***path] = dpm[i - 1][j - 1][***path]
+                                    } else if dpm[i][j][alphas[i]] == u {
+                                        dpm[i][j][***path] = dpm[i - 1][j][***path]
+                                    } else {
+                                        dpm[i][j][***path] = dpm[i][j - 1][***path]
+                                    }
+                                }
+                            }
+                        } else {
+                            let u = dpm[i - 1][j][alphas[i - 1]]
+                                + dpm[i - 1][j][alphas[i]]
+                                + score_matrix.get(&(lnz[i], '-')).unwrap();
+                            let d = dpm[i - 1][j - 1][alphas[i - 1]]
+                                + dpm[i - 1][j - 1][alphas[i]]
+                                + score_matrix.get(&(lnz[i], sequence[j])).unwrap();
+                            let l = dpm[i][j - 1][alphas[i]]
+                                + score_matrix.get(&(sequence[j], '-')).unwrap();
+                            dpm[i][j][alphas[i]] = *[d, u, l].iter().max().unwrap();
+                            for path in x.iter() {
+                                if ***path != alphas[i] {
+                                    if dpm[i][j][alphas[i]] == d {
+                                        dpm[i][j][***path] = dpm[i - 1][j - 1][***path] - dpm[i-1][j-1][alphas[i]]
+                                    } else if dpm[i][j][alphas[i]] == u {
+                                        dpm[i][j][***path] = dpm[i - 1][j][***path] - dpm[i-1][j][alphas[i]]
+                                    } else {
+                                        dpm[i][j][***path] = dpm[i][j - 1][***path]
+                                    }
+                                }
+                            }
+                        }
+                    } else {
+                        // multiple alphas possible
+                        let mut alphas_deltas = HashMap::new();
+                        for p in pred_hash.get(&i).unwrap() {
+                            let curr_node_paths = path_node[i].iter().collect::<HashSet<&usize>>();
+                            let pred_paths = path_node[i - 1].iter().collect::<HashSet<_>>();
+                            let x = curr_node_paths
+                                .intersection(&pred_paths)
+                                .collect::<Vec<_>>();
+                            if x.contains(&&&alphas[*p]) {
+                                let paths = x.iter().map(|p| ***p).collect::<Vec<usize>>();
+                                alphas_deltas.insert(alphas[*p], paths);
+                            } else {
+                                //set new alpha
+                                let temp_alpha = x.iter().min().unwrap();
+                                let paths = x.iter().map(|p| ***p).collect::<Vec<usize>>();
+                                alphas_deltas.insert(***temp_alpha, paths);
+                            }
+                        }
+                    }
                 }
                 _ => {
                     let l =
@@ -226,7 +302,7 @@ pub fn exec(
                                         dpm[i][j][*a] -= dpm[i][j][alphas[i]];
                                         for path in delta.iter() {
                                             if path != a {
-                                                dpm[i][j][*path] -= dpm[i][j][*a];
+                                                dpm[i][j][*path] += dpm[i][j][*a];
                                             }
                                         }
                                     }
@@ -240,8 +316,17 @@ pub fn exec(
     }
     dpm[..dpm.len() - 1]
         .iter()
-        .for_each(|l| println!("{:?}", l[l.len()-1]));
+        .enumerate()
+        .for_each(|(i, line)| println!("{:?}", line[0][alphas[i]]));
     println!("{:?}", alphas);
-    println!("{:?}", path_node); 
-     
+    println!("{:?}", path_node);
+    for (idx, line) in dpm.iter().enumerate() {
+        print!("{}\t", dpm[idx][line.len() - 1][alphas[idx]]);
+        for path in path_node[idx].iter().enumerate() {
+            if path.0 != alphas[idx] {
+                print!("{}\t", dpm[idx][0][path.0] + dpm[idx][0][alphas[idx]]);
+            }
+        }
+        println!();
+    }
 }
