dca5e893fd918cbaaf7e66bd6e6b87cbb808fc49
dcmonti <d.monti11@campus.unimib.it>
2022-09-30 16:25:43 +0200

    bugfix

--- src/main.rs
+++ src/main.rs
@@ -296,14 +296,6 @@ fn main() {
                 utils::write_gaf(&gaf.to_string(), i);
             }
         }
-        10 => {
-            let graph = pathwise_graph::read_graph_w_path(&graph_path, false);
-            let (len, start, end) =
-                recombination_output::get_path_len_start_end(&graph.nodes_id_pos, 0, 2, 3);
-            println!("LEN: {len}");
-            println!("START: {start}");
-            println!("END: {end}");
-        }
         _ => {
             panic!("alignment mode must be 0, 1, 2 or 3");
         }
